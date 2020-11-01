066a65b3125000c819cce6d1865e67f91e0999f0
Philip Holden <philip.holden@woodmac.com>
2020-11-01 17:30:12 +0000

    old work

--- service-worker.js
+++ service-worker.js
@@ -2,13 +2,13 @@ importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
 );
 
-console.log("service-worker", Object.keys(workbox));
+console.log("service-worker");
 
 const c = workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
-console.log(c);
-workbox.skipWaiting();
-workbox.clientsClaim();
-// self.addEventListener("activate", (event) => {
-//   clients.claim();
-//   console.log("Now ready to handle fetches!");
-// });
+
+// workbox.skipWaiting();
+// workbox.clientsClaim();
+self.addEventListener("activate", (event) => {
+  clients.claim();
+  console.log("Now ready to handle fetches!");
+});
