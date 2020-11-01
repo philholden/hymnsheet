0e1d37e10d0f206edbcb38edc0da53e09694ee1a
Philip Holden <philip.holden@woodmac.com>
2020-11-01 13:13:49 +0000

    test css

--- src/common/TitleEditable.svelte
+++ src/common/TitleEditable.svelte
@@ -15,7 +15,7 @@
 <style>
   .buttons {
     display: grid;
-    grid-template-columns: min-content min-content;
+    /* grid-template-columns: min-content min-content; */
     justify-content: space-between;
   }
   .outer {
@@ -23,35 +23,39 @@
     align-items: center;
   }
 </style>
+
 <div class="outer">
   <Title style="padding-right: 8px;">{value}</Title>
   {#if editable}
-  <Button invisible iconLeft="edit" on:click={() => show = true}/>
+    <Button invisible iconLeft="edit" on:click={() => (show = true)} />
   {/if}
 </div>
 {#if show}
-<Modal on:close={() => show = false}>
-  <form on:submit={(e) => {
-    show = false;
-    console.log(onSave)
-    if(value && onSave) onSave(value);
-    e.preventDefault()
-  }}>
-  <Input
-    autoFocus
-    on:input={(e) => value = e.target.value}
-    style="width: 100%;box-sizing: border-box;margin-bottom: 16px"
-    value={value}
-  />
-  <div class="buttons">
-  
- 
-  <Button  type="button" invisible iconLeft="close" on:click={() => show = false}>
-    Close
-  </Button>
-  <Button type="submit">Save</Button>
-  </div>
-  </form>
-  
-</Modal>
+  <Modal on:close={() => (show = false)}>
+    <form
+      on:submit={e => {
+        show = false;
+        console.log(onSave);
+        if (value && onSave) onSave(value);
+        e.preventDefault();
+      }}>
+      <Input
+        autoFocus
+        on:input={e => (value = e.target.value)}
+        style="width: 100%;box-sizing: border-box;margin-bottom: 16px"
+        {value} />
+      <div class="buttons">
+
+        <Button
+          type="button"
+          invisible
+          iconLeft="close"
+          on:click={() => (show = false)}>
+          Close
+        </Button>
+        <Button type="submit">Save</Button>
+      </div>
+    </form>
+
+  </Modal>
 {/if}
