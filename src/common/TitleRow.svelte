6c04a5a8c28a2b8dd0e4df512df7791dbf2a6343
Philip Holden <philip.holden@woodmac.com>
2020-11-01 12:57:29 +0000

    try fix build

--- src/common/TitleRow.svelte
+++ src/common/TitleRow.svelte
@@ -1,12 +1,12 @@
 <style>
-  .titleRow {
+  /* .titleRow {
     height: 48px;
     display: grid;
     gap: 1rem;
     padding: 16px 0 8px;
     grid-template-columns: 1fr min-content;
     align-items: center;
-  }
+  } */
 </style>
 
 <div class="titleRow">
